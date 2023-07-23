f32a977a716ae3dcbbb9e7e37dde3fc05bcf0123
Nidhinsha <nidhinsha64@gmail.com>
2023-07-23 13:29:02 +0530

    backend worked

--- server/src/router/book.ts
+++ server/src/router/book.ts
@@ -1,6 +1,7 @@
-import express, {Express, Request, Response} from 'express';
-const router = express.Router()
+import express, { Router } from 'express';
+const router: Router = express.Router()
 const bookController = require("../controller/bookController")
 
 router.route("/search").get(bookController.searchBook)
+
 module.exports = router
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 require('dotenv').config();
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 const bodyParser = require("body-parser")
 
 const app: Express = express();
-const port = 5000;
+const port: number = 5000;
 app.use(bodyParser.json())
 
 const bookRouter = require("./router/book")
 
 app.use("/api", bookRouter)
 
-app.listen(port, ()=> {
-console.log(`[Server]: I am running at https://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`[Server]: I am running at https://localhost:${port}`);
 });
